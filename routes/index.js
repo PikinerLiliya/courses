53b8e66faeedce0775cbaecfd0fcda963b803fe3
Liliya Mykhailova <liliya.mykhailova@thinkmobiles.com>
2018-05-17 18:04:01 +0300

    add index.html

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,10 @@ module.exports = function (app) {
   app.use('/users', usersRouter);
   app.use('/posts', postsRouter);
 
+  app.get('/', function (req, res, next) {
+    res.sendfile('index.html');
+  });
+
   app.use(function (err, req, res, next) {
     var status = err.status || 500;
 
